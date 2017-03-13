4b27426cb797e6d5e990bee74a62e730d77d3fdb
moncure <amymoncure@gmail.com>
2017-03-13 13:40:08 -0500

    overload the Box constructor;

--- Box.java
+++ Box.java
@@ -15,13 +15,39 @@
 //Box buildBox(Point, int, int)
 
 import java.awt.Point;
-public class Box {
+
+  public class Box {
     
     
     int x1 = 0;
     int y1 = 0;
     int x2 = 0;
     int y2 = 0;
+    
+    Box(int x1, int y1, int x2, int y2) {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }  
+      
+    Box(Point topLeft, Point bottomRight) {
+            x1 = topLeft.x;
+            y1 = topLeft.y;
+            x2 = bottomRight.x;
+            y2 = bottomRight.y;
+        }
+        
+    Box(Point topLeft, int w, int h) {
+            x1 = topLeft.x;
+            y1 = topLeft.y;
+            x2 = x1 + w;
+            y2 = y1 + h;
+        }
+    
+    
+    
+    
     
     Box buildBox(int x1, int y1, int x2, int y2)
     {
@@ -32,6 +58,12 @@ public class Box {
         
         return this;
     }
+    
+    
+    
+    
+    
+    
     
     Box buildBox(Point topLeft, Point bottomRight)
     {
@@ -63,7 +95,7 @@ public class Box {
     {
         Box rect = new Box();
         
-        System.out.println("Calling buildBox with coordinates " + "(25,25) and (50,50)");
+        System.out.println("Calling buildBox with coordinates " + "(25,25) and (50,50) passing four int parameters");
         rect.buildBox(25,25,50,50);
         rect.printBox();
         
@@ -74,6 +106,18 @@ public class Box {
         System.out.println("\nCalling buildBox with 1 point (10,10), width 50, and height 50");
         rect.buildBox(new Point (10,10), 50, 50);
         rect.printBox();
+        
+        
+        Box rect2;
+            
+                       
+            System.out.println("Calling Box with coordinates (10,10) and (20,20) passing two Point parameters");
+            rect2 = new Box(new Point(10,10),new Point(20,20));
+            rect2.printBox();
+            
+            System.out.println("Calling Box with coordinates (10,10) and width 50 and height 50 passing a Point and two int parameters");
+            rect2 = new Box(new Point(10,10),50,50);
+            rect2.printBox();
         
                
     }
